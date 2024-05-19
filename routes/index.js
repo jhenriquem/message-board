73e14630ea400973a5e25e4c637e8e91caffbe07
jhenriquem <machadojh11@gmail.com>
2024-05-19 20:17:30 -0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var router = express.Router();
+
+const messagesArray = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { messages: messagesArray });
+});
+
+router.post("/", (req, res, next) => {
+  messagesArray.push({
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date(),
+  });
+  res.redirect("/");
+});
+
+module.exports = router;

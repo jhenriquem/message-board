9b3d400d73e9a6a1958248df9387b02efe0e82e3
jhenriquem <machadojh11@gmail.com>
2024-08-17 11:50:19 -0300

    (api) basic structure

---
+++ server/routes/messagesRoutes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+const router = Router()
+
+router.get("/", (req, res) => {
+  res.send("HI!")
+})
+
+router.post("/", (req, res) => {
+
+  res.send("Await minute!")
+})
+
+export default router
---
+++ server/src/server.ts
@@ -0,0 +1,46 @@
+import express, { Application } from "express"
+import cors from "cors"
+import bodyParser from "body-parser"
+import morgan from "morgan"
+import dotenv from "dotenv"
+import router from "../routes/messagesRoutes"
+
+export default class Server {
+  private app: Application
+  private port: number | string
+  private paths = {
+    messages: "/api/messages"
+  }
+
+  constructor() {
+    this.app = express()
+    this.port = process.env.PORT || 8080
+
+    dotenv.config()
+
+    this.middlewares();
+    this.routes()
+  }
+
+  middlewares() {
+    this.app.use(morgan("dev"));
+    this.app.use(cors());
+    this.app.use(bodyParser.json());
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use(this.paths.messages, router)
+  }
+
+  online() {
+    try {
+      this.app.listen(this.port, () => {
+        console.log(`Server is online on port ${this.port}`);
+      })
+    }
+    catch (err: any) {
+      console.error(`Error: ${err.message}`);
+    }
+  }
+}

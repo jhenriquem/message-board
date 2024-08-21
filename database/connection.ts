e16ea1ed688f13a0511694771ff0c7ab39667645
jhenriquem <machadojh11@gmail.com>
2024-08-20 22:34:59 -0300

    (api) database configuration and basic queries

--- database/connection.ts
+++ database/connection.ts
@@ -1,9 +1,11 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  host: process.env.POSTGRES_HOST,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  port: process.env.POSTGRES_PORT,
+  host: process.env.POSTGRES_HOST as string,
+  user: process.env.POSTGRES_USER as string,
+  password: `${process.env.POSTGRES_PASSWORD}`,
+  database: process.env.POSTGRES_DATABASE as string,
+  port: process.env.POSTGRES_PORT as unknown as number
 })
+
+export default pool
---
+++ database/queries.ts
@@ -0,0 +1,3 @@
+export const queryAllMessages = `SELECT * FROM ${process.env.POSTGRES_TABLENAME}`
+export const insertNewMessage = `INSERT INTO ${process.env.POSTGRES_TABLENAME}(id,username,message,date) VALUES ($1,$2,$3,$4)`
+

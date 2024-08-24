1e49ad885db92921edd1d213da2ca648ddd001ba
jhenriquem <machadojh11@gmail.com>
2024-08-23 21:35:40 -0300

    (api) creating middleware to verify data

--- database/queries.ts
+++ database/queries.ts
@@ -1,3 +1,2 @@
-export const queryAllMessages = `SELECT * FROM ${process.env.POSTGRES_TABLENAME}`
-export const insertNewMessage = `INSERT INTO ${process.env.POSTGRES_TABLENAME}(id,username,message,date) VALUES ($1,$2,$3,$4)`
-
+export const queryAllMessages = `SELECT * FROM ${process.env.POSTGRES_TABLENAME}`;
+export const insertNewMessage = `INSERT INTO ${process.env.POSTGRES_TABLENAME}(id,username,message,date) VALUES ($1,$2,$3,$4)`;
--- routes/messagesRoutes.ts
+++ routes/messagesRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getMessages, postMessage } from "../controllers/messagesControllers";
+import checkData from "../middlewares/checkData";
 
 const router = Router();
 
 router.get("/", getMessages);
 
-router.post("/", postMessage);
+router.post("/", checkData, postMessage);
 
 export default router;
